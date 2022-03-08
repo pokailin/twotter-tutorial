3499bf7dea3f6e6fef00bf472f8808e950b09113
pokailin <56913442+pokailin@users.noreply.github.com>
2022-03-08 22:16:02 +0800

    initialize repository
    init files

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="app">
+    @{{ user.username }} - {{ fullName }}
+    <strong>Followers</strong> {{ followers }}
+    <button @click="followUser">Follow</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      followers: 0,
+      user: {
+        id: 1,
+        username: '_pokailin',
+        firstName: 'Pokai',
+        lastName: 'Lin',
+        email: 'fakeemail@fakeemail.com',
+        isAdmin: true
+      }
+    }
+  },
+  watch: {
+    followers(newFollowerAccount, oldFollowerAccount) {
+      if (oldFollowerAccount < newFollowerAccount) {
+        console.log(`${this.user.username} has gained a follower!`)
+      }
+    }
+  },
+  computed: {
+    fullName() {
+      return `${this.user.firstName} ${this.user.lastName}`
+    }
+  },
+  methods: {
+    followUser() {
+      this.followers++
+    }
+  },
+  mounted() {
+    this.followUser()
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+}
+</style>
